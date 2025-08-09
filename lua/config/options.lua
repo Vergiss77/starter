d30b6020318d0638ef390947b766eae3d8a5e85e
Vergissmeinnicht <2547405171@qq.com>
2025-08-09 22:26:58 +0800

    feat: add new settings.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+-- 禁用相对行号
+opt.relativenumber = false
+-- 设置tab缩进为4
+opt.tabstop = 4
+opt.shiftwidth = 4
